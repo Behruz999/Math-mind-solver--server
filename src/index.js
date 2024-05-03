afefa3dff185e273fdd8f6f3a29fec12b8da71d4
DarksideEvil <sinister58450@gmail.com>
2024-05-03 22:48:16 +0500

    intial & last

--- src/index.js
+++ src/index.js
@@ -1,11 +1,17 @@
 const express = require("express");
 const app = express();
 const mainRouter = require("./mainRouter");
-const secure = require('./private/secure')
-secure(app)
+const secure = require("./private/secure");
+secure(app);
 app.use(express.json());
 
-app.use("/api", mainRouter);
+app.use("/", mainRouter);
+
+// Error handling middleware
+app.use((err, req, res, next) => {
+  console.error(err);
+  res.status(500).json({ message: "Internal server error" });
+});
 
 app.listen(process.env.PORT || 5000, () => {
   console.log(`${process.env.PORT || 5000}'s port online...`);
