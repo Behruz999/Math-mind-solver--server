5b7e411b54cc3c65750a9062db06bd5f2462212b
DarksideEvil <sinister58450@gmail.com>
2024-05-15 17:41:03 +0500

    last one

--- src/private/secure.js
+++ src/private/secure.js
@@ -5,9 +5,26 @@ const allowedOrigins = [
 ];
 
 module.exports = (app) => {
+  // app.use(
+  //   cors({
+  //     origin: allowedOrigins,
+  //     optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
+  //     methods: ["GET", "POST"],
+  //     allowedHeaders: ["Content-Type", "Authorization"],
+  //     credentials: true,
+  //   })
+  // );
+
   app.use(
     cors({
-      origin: allowedOrigins,
+      origin: function (origin, callback) {
+        // Check if the origin is in the allowed origins array or if it's undefined (which happens for same-origin requests)
+        if (!origin || allowedOrigins.includes(origin)) {
+          callback(null, true);
+        } else {
+          callback(new Error("Not allowed by CORS"));
+        }
+      },
       optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
       methods: ["GET", "POST"],
       allowedHeaders: ["Content-Type", "Authorization"],
